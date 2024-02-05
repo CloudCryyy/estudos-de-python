8c8e87f1c817a33ce5e8d7b716c6e66fa46f80a6
CloudCryyy <cloud.cryy@gmail.com>
2024-02-04 21:00:24 -0300

    estudos de listas

---
+++ listas.py
@@ -0,0 +1,26 @@
+lista = ["chicago", "queens", "salvador", "pernambuco"]
+
+lista2 = [2, 3, 4, 5, 6]
+
+lista3 = [2.0, 3.5, 7.9]
+lista2 = lista2 + lista3
+print(sum(lista3))
+print(max(lista3))
+print(min(lista3))
+
+nome = "curso de python"
+valor = range(10)
+
+lista4 = list(nome)
+print(lista4)
+lista4 = list(valor)
+print(lista4)
+
+lista5 = ["cachorro", "gato", "cavalo", "girafa", "arraia"]
+print(lista5)
+lista5[2] = "vaca"
+print(lista5)
+
+lista5[1:4] = ["águia", "tubarão", "caracol"]
+
+print(lista5)
